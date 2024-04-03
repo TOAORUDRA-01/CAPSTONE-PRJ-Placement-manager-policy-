fe7368433d41b83bcd31efad75e4da12e8d4bea3
Vpoorva-03 <202301179@daiict.ac.in>
2024-04-03 19:26:04 +0530

    adding show and modify functions

--- Classes/internship.h
+++ Classes/internship.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include<vector>
 using namespace std;
 #ifndef Internship_H
 #define Internship_H
@@ -11,6 +12,7 @@ class Internship{
         string type;
         
         public:
+        vector<Internship> internships;
         Internship(string name, int duration, string type) {
         this->name = name;
         this->duration = duration;
@@ -18,6 +20,45 @@ class Internship{
     }
     friend class Student;
     friend ostream& operator<<(ostream& ,Internship& internship);
+    void add_internships()
+    {
+
+        bool flag = true;
+        string name, type; 
+        int seats,duration ;
+        char c;
+        cout << "  Enter the internship details which you want to add:" << endl;
+
+        while (flag)
+        {
+            cout << "  Enter the internship name: ";
+            cin >> name;
+            cout << "  Enter the internship type: ";
+            cin >> type;
+            cout << "  Enter the duration of internship: ";
+            cin >> duration;
+            cout << "  Do you want to add more internships? (y/n): ";
+            cin >> c;
+            Internship *inter = new Internship(name,duration,type);
+            internships.push_back(*inter);
+            if (c == 'n')
+            {
+                flag = false;
+            }
+        }
+    }
+
+    // Other member functions
+    void show_internship(){
+        cout << "\t\t\t\t\t\tINTERNSHIP DETAILS:" << endl;
+        cout << "\n\t\tName \t\t\t\t\t\t Duration(in weeks) \t\t\t\t\t\t Type" << endl;
+        cout << "---------------------------------------------------------------------------------------------------" << endl;
+int index = 0;
+        for(int i = 0; i<internships.size();i++){
+            cout << index++ << "||" << "\t\t" << internships[i].name << "\t\t\t\t\t\t" << internships[i].duration << "\t\t\t\t\t\t" << internships[i].type  << endl;  
+        }
+    }
+    
     
 }; 
 ostream& operator<<(ostream& out,Internship& internship){
